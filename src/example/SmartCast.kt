83f66625ad95bc40eeccee3818a6ceee826e909d
gamgoon <gamgoon@gmail.com>
2018-12-03 04:37:38 +0900

    if -> when

--- src/example/SmartCast.kt
+++ src/example/SmartCast.kt
@@ -13,6 +13,8 @@ fun main(args: Array<String>) {
 }
 
 fun eval(e: Expr): Int =
-    if (e is Num) e.value
-    else if (e is Sum) eval(e.right) + eval(e.left)
-    else throw IllegalArgumentException("Unknown expression")
+    when(e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
